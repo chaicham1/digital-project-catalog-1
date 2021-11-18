ae739fbd00b01719c1479ad2ffc145c147494007
chaicham1 <94437356+chaicham1@users.noreply.github.com>
2021-11-18 15:27:25 +0200

    changes

--- backend/routes/projectsInfoRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-const checkAuth = require("../../middleware/auth");
-
-const appointmentsController = require('../../controllers/appointmentsController/appointmentsController')
-
-Router.get('/getProjectsInfo',checkAuth.authorization,appointmentsController.getAppointmentDetails);
